af625d12a0cb5f400ea33530ea6d8e5700a4d88f
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-22 16:37:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,27 @@ class LambdaDemo extends Component {
     this.state = { loading: false, msg: null }
   }
 
- 
-  render() {
-  }
-    return(
+  handleClick = api => e => {
+    e.preventDefault()
 
-    );
+    this.setState({ loading: true })
+    fetch("/.netlify/functions/" + api)
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, msg: json.msg }))
+  }
+
+  render() {
+    const { loading, msg } = this.state
+
+    return (
+      <p>
+        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
+        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+        <br />
+        <span>{msg}</span>
+      </p>
+    )
+  }
 }
 
 class App extends Component {
