b28554e24b6f5d86ca7613a5cce5df59ff185264
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-22 16:20:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,8 +35,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
           <LambdaDemo />
+        </header>
       </div>
     )
   }
