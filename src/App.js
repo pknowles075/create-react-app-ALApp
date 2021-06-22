299a10263cf223fc06c29df4cf38f87beffd8526
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-22 16:07:48 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,44 +8,23 @@ class LambdaDemo extends Component {
     this.state = { loading: false, msg: null }
   }
 
-  handleClick = api => e => {
-    e.preventDefault()
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
 
   render() {
-    const { loading, msg } = this.state
 
     return (
+      <>
       <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
+      <LambdaDemo />
+
+       
       </p>
+      <a>Layan</a>
+      </>
     )
   }
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a>This is a test</a>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
+
 
 export default App
