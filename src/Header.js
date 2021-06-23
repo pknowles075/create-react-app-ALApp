a93a981d5904c672800de1e7754e9ca453bfc9a3
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-23 00:44:28 -0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -17,7 +17,7 @@ export const Header = () => {
         <div onClick={()=>setShowCardGrammar(!ShowCardGrammar)} id="divItems">Grammar</div>{}
         <div onClick={()=>setShowCardVoacb(!ShowCardVocab)} id="divItems">Vocab</div>{}
         {ShowCard && <FlashCards />}
-        {ShowCardGrammar && ShowCard ===false && <Grammer /> }
+        {ShowCardGrammar && ShowCard  && <Grammer /> }
         {ShowCardVocab && ShowCardGrammar ===false &&<Voacb /> }
       </div>
       </>
