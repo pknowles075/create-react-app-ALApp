73bea9865bd7d38cd7224520788dae79ff2d1d86
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-22 19:53:24 -0500

    v2

--- src/FlashCards.js
+++ src/FlashCards.js
@@ -1,20 +1,27 @@
 import "./FlashCards.css"
 import React, {useState} from 'react';
 
-function fCF(){
+function fCF(any){
     var lEng=["White","Yellow","Blue"];
     var lAra=["Abyad","Asfar","Azraq"];
     let c=Math.floor(Math.random()*lEng.length)
-    var combine =lEng[c]+" "+lAra[c];
+
+    if(any==0){
+        var combine = lAra[c];
+    }
+    if(any==1){
+        var combine =lEng[c];
+    }
+    
     return combine;
 }
 export const FlashCards = () => {
-    const [count, setCount]=useState(fCF);
+    const [count, setCount]=useState(fCF(1));
     return (
         <div className="main">
             <div id="flashCard">
-                <div id="x" onClick={()=> setCount(fCF())}>{count}</div>
-
+                <div id="x" onClick={()=> setCount(fCF(0))}>{count}</div>
+                <div id="nextButton" onClick={()=> setCount(fCF(1))}>Next</div>
             </div>
             
         </div>
