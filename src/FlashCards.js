5d521ed85c4942724d0c3104f785caa15837e4d3
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-22 23:13:00 -0500

    test

--- src/FlashCards.js
+++ src/FlashCards.js
@@ -5,13 +5,19 @@ function animalF(){
   
     document.getElementById("info").innerText="[animal info here]"
 }
+function carF(){
+  
+    document.getElementById("info").innerText="[car info here]"
+}
 export const FlashCards = () => {
 
     return (
         <div className="main">
           
-                    <div id="flashCard" onClick={animalF}>
-                        <div id="flashCardText"><span role="img" aria-label="string">📅</span>June 22,2021 Animals</div>
+                    <div id="flashCard" >
+                        <div id="flashCardText"onClick={animalF}><span role="img" aria-label="string">📅</span>June 22,2021 Animals</div>
+                        <div id="flashCardText"onClick={carF}><span role="img" aria-label="string">📅</span>June 22,2021 Cars</div>
+                   
                     <br>
                     </br>
                     <div id="info"></div>
