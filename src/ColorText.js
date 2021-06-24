b6e1f4c92f2050bf3ea5a2f0190d68dc9eeb0df5
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-23 21:47:21 -0500

    Css upates

--- src/ColorText.js
+++ src/ColorText.js
@@ -4,11 +4,19 @@ export const ColorText = () => {
 
     return (
         
-        <h1 id="infoBox">
-            Animal
-            Arabic:kalab<br></br>
-            Alharuf:الكلب<br></br>
-            English:Dog<br></br>
+        <h1 id="infoBox">  Animals
+            <table>
+              
+                <tr>
+                    <th>Arabic</th>
+                    <th>English</th>
+                </tr>
+                <tr>
+                    <td>Kalb</td>
+                    <td>Dog</td>
+                </tr>
+            </table>
+
         </h1>
     )
 }
