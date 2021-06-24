7f4ee4bbd7380b5c584e1363a213be0347007ca2
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-23 20:00:40 -0500

    Css Update more

--- src/FlashCards.js
+++ src/FlashCards.js
@@ -1,25 +1,28 @@
 import "./FlashCards.css"
 import React   from 'react';
-import  ColorText from "./ColorText" 
+import  ColorText from "./ColorText";
+import FoodText from "./FoodText";
 import { useState } from "react";
 
 export const FlashCards = () => {
     const [ShowCard,setShowCard]=useState(false);
+    const [ShowFood,setFood]=useState(false);
 
     return (
         <>
         <div className="main">
           <div id="flTitle">CATEGORY ARCHIVES: LEARN ARABIC VOCABULARY</div>
                     <div id="flashCard" >
-                        <div id="flashCardText"onClick={()=>setShowCard(!ShowCard)}><span role="img" aria-label="string">📅</span>June 22,2021 Animals</div>
-                        <div id="flashCardText"onClick={()=>setShowCard(!ShowCard)}><span role="img" aria-label="string">📅</span>June 22,2021 Animals</div>
+                        <div id="flashCardText"onClick={()=>setShowCard(!ShowCard)}><span role="img" aria-label="string">📅</span>June 22,2021 Color</div>
+                        <div id="flashCardText"onClick={()=>setFood(!ShowFood)}><span role="img" aria-label="string">📅</span>June 22,2021 Food</div>
 
                     <br>
                     </br>
 
                     </div>
                     <div id="info">{ShowCard  &&<ColorText/>}</div>
-          
+                    <div id="info">{ShowFood  &&<FoodText/>}</div>
+
             </div>
             </>
             
---
+++ src/FoodText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FoodText = () => {
+    return (
+        <h1 id="infoBox">
+        Not done<br></br>
+        Joe Mama
+        </h1>
+    )
+}
+
+export default FoodText
