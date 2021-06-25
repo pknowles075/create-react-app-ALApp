50253794d9ba3db364cf821627b1a204f29070a4
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-24 19:09:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component} from "react"
 import "./App.css"
 import Header from "./Header.js"
-import FlashCards from "./FlashCards"
-import HomePage from "./HomePage"
+
 class App extends Component {
   render() {
     return (
