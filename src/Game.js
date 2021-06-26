b2637e50a2e661c784720cdbe09f02b583862f40
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-25 21:16:03 -0500

    Js game

---
+++ src/Game.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import "./Game.css";
+import { useState } from 'react';
+
+
+var  Random_Quote_Num=Math.floor(Math.random()*2);
+
+const Game = () => {
+    const [selected, setSelected] = useState(Random_Quote_Num)
+
+    function rollIt(){
+        const Random_Quote_Num = Math.floor(Math.random() * 2);
+        setSelected(Random_Quote_Num);   
+     }
+    const Arabic_Text=
+    ["أنا أحب القطط","للبيت",];
+   
+   
+    const Eng_Text=
+    ["I love cat","I go home"];
+
+
+
+   
+
+
+
+   
+
+
+var CHOOSEN_TEXT=Arabic_Text[selected];
+var CHOOSEN_TEXT_Eng=Eng_Text[selected];
+
+    var score=0;
+    function Score(){
+        
+        let CORRECT=false;
+        if(CHOOSEN_TEXT_Eng===username){
+            score+=1000;
+            CORRECT=true;
+        }
+        alert(
+            "Your trans "+
+            username+"\nCorrect trans "+CHOOSEN_TEXT_Eng+
+            "\nYour "+CORRECT+"\nYour score is:"+score);
+        Random_Quote_Num=Math.floor(Math.random()*2);
+    }
+
+    function useInput({ type /*...*/ }) {
+        
+        const [value, setValue] = useState("");
+        const input = <input value={value} onChange={e => setValue(e.target.value)} type={type} />;
+        return [value, input];
+      }
+
+    const [username, userInput] = useInput({ type: "text" });
+    return (
+        <>
+        <div>Translate this</div><br></br>
+        <div>"id:"{selected}</div>
+        <div id="arabicText"><p>{CHOOSEN_TEXT}</p></div>
+       
+        {userInput}  {username} <br />
+        <button onClick={Score} >Done!</button>
+        <button onClick={rollIt} >New!</button>
+
+    </>
+    )
+}
+
+export default Game;
--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,7 @@ import Grammer from './Grammer';
 import Voacb  from './Voacb';
 import LinkInfo from './LinkInfo';
 import HomePage from './HomePage';
+import Game from './Game';
 export const Header = () => {
     const[active, setActive]=useState("FinalCard")
 
@@ -19,12 +20,14 @@ export const Header = () => {
                         <div id="divItems"onClick={()=>setActive("2Card")}>Grammer</div>
                         <div id="divItems"onClick={()=>setActive("3Card")}>Voacb</div>
                         <div id="divItems"onClick={()=>setActive("4Card")}>Link</div>
+                        <div id="divItems"onClick={()=>setActive("5Card")}>Game</div>
 
                     {active ==="0Card" && <HomePage/>}
                     {active ==="1Card" && <FlashCards/>}
                     {active ==="2Card" && <Grammer/>}
                     {active ==="3Card" && <Voacb/>}
                     {active ==="4Card" && <LinkInfo/>}
+                    {active ==="5Card" && <Game/>}
 
       </div>
       </>
