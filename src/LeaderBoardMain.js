42d6ee05a7544b86933e45d13f15f2b8989db5f5
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-26 19:46:56 -0500

    Update LeaderBoardMain.js

--- src/LeaderBoardMain.js
+++ src/LeaderBoardMain.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import './LeaderBoardMain.css';
 function LeaderBoardMain() {
-    const score=localStorage.getItem('myData');
+    var score=localStorage.getItem('myData');
+    if(score===null){
+        score="Oh no you have no score yet :("
+    }
     console.log("hi")
     return (
         <div id="mainScore">
