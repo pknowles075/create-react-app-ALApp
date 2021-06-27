84e7ca58096e6a33b4c596aab12776ecb5bb6a35
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-27 11:19:42 -0500

    css massive update

--- src/Game.js
+++ src/Game.js
@@ -16,10 +16,13 @@ var Random_Quote_Num = Math.floor(Math.random() * OFFICAL_NUM);
 
 const Game = () => {
     var [currentScore, setScore] = useState(0)
+
     if(localStorage.getItem('myData')===null){
         localStorage.setItem('myData', currentScore);
         console.log("set")
         }
+  
+             
     const [selected, setSelected] = useState(Random_Quote_Num)
 
     function rollIt() {
@@ -29,28 +32,60 @@ const Game = () => {
 
 
 
+const  mydiv = document.createElement("DIV");
+mydiv.setAttribute("id","wrongId");
 
 
+function ShowWrong(){
+    mydiv.innerHTML = "Wrong";                   
+    document.body.appendChild(mydiv);
+}
 
+function RemoveWrong(){
+    
+    document.body.removeChild(mydiv);
 
+}
 
-
-
+function ShowCorrect(){
+ mydiv.innerHTML = "Correct";  
+ mydiv.setAttribute("id","correctId");
+ document.body.appendChild(mydiv);
+}
     var CHOOSEN_TEXT = Arabic_Text[selected];
     var CHOOSEN_TEXT_Eng = Eng_Text[selected];
 
 
     const [value, setValue] = useState("");
     let CORRECT = false;
-
+  
+   function runner(event){
+    if(event.key === 'Enter'){
+        console.log('enter press here! ')
+        Score();
+        rollIt();
+    }
+    else{}
+   }
     function Score() {
+        console.log("score was running")
         let CORRECT = false;
         setValue('');
+       
+          
+          
         if (CHOOSEN_TEXT_Eng === username) {
             currentScore =currentScore+ 100;
             CORRECT = true;
             setScore(currentScore);
-           
+            ShowCorrect();
+            setTimeout(RemoveWrong,1000)
+
+        }
+        else{
+            ShowWrong();
+            setTimeout(RemoveWrong,1000)
+
         }
         if(localStorage.getItem('myData')<currentScore){
             localStorage.setItem('myData', currentScore);
@@ -62,31 +97,33 @@ const Game = () => {
         Random_Quote_Num = Math.floor(Math.random() * 2)
         return setScore(currentScore);
     }
-
+       
+  
     function useInput({ type /*...*/ }) {
 
-        const input = <input value={value} onChange={e => setValue(e.target.value)} type={type} />;
+        const input = <input id="inputBox"value={value}  onKeyPress={runner} autoFocus="autoFocus" onChange={e => setValue(e.target.value)} type={type} />;
         return [value, input];
     }
 
     var [username, userInput] = useInput({ type: "text" });
-
+  
     const p=<p>
             Your Translate {username} Correct Translate {CHOOSEN_TEXT_Eng} 
             Your {CORRECT} Your score is: {currentScore}</p>
     return (
         <>
-            <div id="titleDiv"><p>Translate this your score is: {currentScore}</p></div><br></br>
-    
-            <div id="arabicText"><p>{CHOOSEN_TEXT}</p></div>
-            <div id="hiddenCorrect"><p>[Dev_Tools:{CHOOSEN_TEXT_Eng}]</p></div>
-            <div>id:{selected}</div>
+        <div id="correct">Correct</div>
+        <div id="arabicText"><p>{CHOOSEN_TEXT}</p></div>
+        {username}<br></br>
+            {userInput}<br></br>
+            <button  onClick={() => {Score(); rollIt();}}  id="thisButton"><p>Reload</p></button>
 
-            {username}<br></br>
-            {userInput}   <br />
-            <button onClick={() => {Score(); rollIt();}}  >Done!</button>
-            <button onClick={rollIt} >New!</button>
-            {p}
+            <div id="titleDiv"><h5>Result</h5><p> {currentScore} CTP</p><h6>{CHOOSEN_TEXT_Eng }<br></br>id: {selected}</h6>
+</div><br></br>
+     
+
+           
+            
         </>
     )
 }
