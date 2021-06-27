10ed8bb4df077f6ad7c8edee29ee8ef68278ba9d
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-06-26 19:10:08 -0500

    js update

--- src/Game.js
+++ src/Game.js
@@ -1,70 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "./Game.css";
-import { useState } from 'react';
 
-const Arabic_Text=
-    ["احتاج طعاما","أنا أحب القطط"];
-const Eng_Text=
-    ["I love cat","I love food"];
 
-    const OFFICAL_NUM=Arabic_Text.length;
-var  Random_Quote_Num=Math.floor(Math.random()*OFFICAL_NUM);
+
+const Arabic_Text =
+    ["ahib kalba","hubu qita","ana akil alluhum","alshaati harin jidana"];
+
+
+
+const Eng_Text =
+    ["I love dog", "love cat","I eat meat","beach is very hot"];
+
+const OFFICAL_NUM = Arabic_Text.length;
+var Random_Quote_Num = Math.floor(Math.random() * OFFICAL_NUM);
 
 const Game = () => {
     const [selected, setSelected] = useState(Random_Quote_Num)
 
-    function rollIt(){
+    function rollIt() {
         const Random_Quote_Num = Math.floor(Math.random() * OFFICAL_NUM);
-        setSelected(Random_Quote_Num);   
-     }
-    
-
-
-   
+        setSelected(Random_Quote_Num);
+    }
 
 
 
-   
 
 
-var CHOOSEN_TEXT=Arabic_Text[selected];
-var CHOOSEN_TEXT_Eng=Eng_Text[selected];
 
-    var score=0;
-    function Score(){
-        
-        let CORRECT=false;
-        if(CHOOSEN_TEXT_Eng===username){
-            score+=1000;
-            CORRECT=true;
+
+
+
+
+    var CHOOSEN_TEXT = Arabic_Text[selected];
+    var CHOOSEN_TEXT_Eng = Eng_Text[selected];
+
+    var [currentScore, setScore] = useState(0)
+
+    const [value, setValue] = useState("");
+    let CORRECT = false;
+
+    function Score() {
+        let CORRECT = false;
+        setValue('');
+        if (CHOOSEN_TEXT_Eng === username) {
+            currentScore =currentScore+ 100;
+            CORRECT = true;
+            setScore(currentScore);
         }
-        alert(
-            "Your trans "+
-            username+"\nCorrect trans "+CHOOSEN_TEXT_Eng+
-            "\nYour "+CORRECT+"\nYour score is:"+score);
-        Random_Quote_Num=Math.floor(Math.random()*2);
+        console.log("score was calc\n"+currentScore)
+        
+        Random_Quote_Num = Math.floor(Math.random() * 2)
+        return setScore(currentScore);
     }
 
     function useInput({ type /*...*/ }) {
-        
-        const [value, setValue] = useState("");
+
         const input = <input value={value} onChange={e => setValue(e.target.value)} type={type} />;
         return [value, input];
-      }
+    }
 
-    const [username, userInput] = useInput({ type: "text" });
+    var [username, userInput] = useInput({ type: "text" });
+
+    const p=<p>
+            Your Translate {username} Correct Translate {CHOOSEN_TEXT_Eng} 
+            Your {CORRECT} Your score is: {currentScore}</p>
     return (
         <>
-        <div id="titleDiv"><p>Translate this</p></div><br></br>
-        <div id="arabicText"><p>{CHOOSEN_TEXT}</p></div>
-        <div>id:{selected}</div>
+            <div id="titleDiv"><p>Translate this your score is: {currentScore}</p></div><br></br>
+    
+            <div id="arabicText"><p>{CHOOSEN_TEXT}</p></div>
+            <div id="hiddenCorrect"><p>[Dev_Tools:{CHOOSEN_TEXT_Eng}]</p></div>
+            <div>id:{selected}</div>
 
-        {username}<br></br>
-        {userInput}   <br />
-        <button onClick={Score} >Done!</button>
-        <button onClick={rollIt} >New!</button>
-
-    </>
+            {username}<br></br>
+            {userInput}   <br />
+            <button onClick={() => {Score(); rollIt();}}  >Done!</button>
+            <button onClick={rollIt} >New!</button>
+            {p}
+        </>
     )
 }
 
