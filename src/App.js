f56aca2f5f77a59ff0b499c77144ba69e7f1366a
pknowles075 <86132696+pknowles075@users.noreply.github.com>
2021-07-14 19:12:10 -0500

    js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component} from "react"
+import React, { useEffect} from "react"
 import "./App.css"
 import Header from "./Header.js"
 import ReactGA from 'react-ga';
 
-class App extends Component {
- 
-  render() {
-      ReactGA.initialize('G-NBQ2D3WLWX');
-      ReactGA.pageview(window.location.pathname + window.location.search)
+function App() {
+  useEffect(()=>{
+    ReactGA.initialize('G-NBQ2D3WLWX');
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }
+        )
+             
   
     return (
       <>
@@ -18,6 +20,5 @@ class App extends Component {
       </>
     )
   }
-}
 
 export default App
